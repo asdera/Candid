5f79e7dd7c1106ed393e17338f018970b09337b5
Andrew Wang <poiuytandrew@gmail.com>
2019-10-23 12:20:39 -0400

    change colour

--- App.js
+++ App.js
@@ -53,6 +53,6 @@ function handleFinishLoading(setLoadingComplete) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff1',
   },
 });
